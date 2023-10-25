26961ee8f2f54b5bd2eb543b30373f380797adb5
Marshallramdh <marshall.120140017@student.itera.ac.id>
2023-10-25 16:17:38 +0700

    popup

--- resources/views/arsip.blade.php
+++ resources/views/arsip.blade.php
@@ -24,33 +24,59 @@
                 </div>
             </div>
             
-            <!-- Pop-up container -->
-            <div id="popupContainer" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 999;">
-                <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border-radius: 10px;">
-                    <!-- Your pop-up content goes here -->
-                    <h2>Tambah Arsip Baru</h2>
-                    <div class="card" style="padding: 20px;">
-                        <form>
-                            <div class="form-group">
-                                <label for="namaArsip">Nama Arsip</label>
-                                <input type="text" class="form-control" id="namaArsip" placeholder="Masukkan nama arsip">
+<!-- Pop-up container -->
+<div id="popupContainer" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 999;">
+    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border-radius: 10px;">
+        <!-- Tombol Close (X) -->
+        <button id="closePopupButton" style="position: absolute; top: 10px; right: 10px; background: none; border: none; font-size: 20px; cursor: pointer;">&times;</button>
+        <!-- Your pop-up content goes here -->
+        <h2>Tambah Arsip Baru</h2>
+        <div class="card" style="padding: 20px; background-color: #f5f5f5;">
+            <form>
+                <div class="form-group">
+                    <div class="wrapper">
+                        <div class="container">
+                          <p>Silakan upload file dengan format PDF</p>
+                          <div class="upload-container">
+                            <div class="border-container">
+                              <div class="icons fa-4x">
+                                <i class="fas fa-file-image" data-fa-transform="shrink-3 down-2 left-6 rotate--45"></i>
+                                <i class="fas fa-file-alt" data-fa-transform="shrink-2 up-4"></i>
+                                <i class="fas fa-file-pdf" data-fa-transform="shrink-3 down-2 right-6 rotate-45"></i>
+                              </div>
+                              <!--<input type="file" id="file-upload">-->
+                              <p>Drag and drop files here, or 
+                                <input type="file" class="form-control-file" id="dokumen" name="dokumen" style="margin-left: 100px;">
                             </div>
-                            <div class="form-group">
-                                <label for="kategoriArsip">Kategori Arsip</label>
-                                <select class="form-control" id="kategoriArsip">
-                                    <option value="kategori1">Kategori 1</option>
-                                    <option value="kategori2">Kategori 2</option>
-                                    <option value="kategori3">Kategori 3</option>
-                                </select>
-                            </div>
-                        </form>
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" id="namaArsip" placeholder="Nama Arsip">
                     </div>
-                    <div class="text-center mt-3">
-                        <button id="saveButton" class="btn btn-primary">Simpan</button>
-                        <button id="cancelButton" class="btn btn-danger ml-2">Batal</button>
+                    <div class="form-group col-md-6">
+                        <select class="form-control" id="kategoriArsip">
+                            <option value="kategori1">Kategori Arsip</option> 
+                            <option value="kategori2">Perizininan</option>
+                            <option value="kategori3">SIP (surat izin praktik)</option>
+                            <option value="kategori3">STR (surat tanda regist)</option>
+                            <option value="kategori3">PKWT</option>
+                            <option value="kategori3">MoU dokter spesialis</option>
+                            <option value="kategori3">Kontrak part time</option>
+                        </select>
                     </div>
                 </div>
-            </div>
+            </form>
+        </div>
+        <div class="text-center mt-3">
+            <button id="saveButton" class="btn btn-primary">Save</button>
+            <button id="cancelButton" class="btn btn-danger ml-2">Cancel</button>
+        </div>
+    </div>
+</div>
+
             
             <script>
                 const openPopupButton = document.getElementById("openPopupButton");
