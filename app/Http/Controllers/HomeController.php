5aa444422b807dab60aab553bf2739eb8d9d971f
Muhammad Ibnu Prayogi <89898918+IbnuPrayogi@users.noreply.github.com>
2023-10-26 07:26:52 +0700

    logout and role check

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        if (Auth::user()->role == 1) {
         return view('home');
-    }
+        }
+        else if (Auth::user()->role == 2) {
+        return view('dashboardkabag');
+        }
+}
 }
