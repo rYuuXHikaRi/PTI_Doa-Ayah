1cf0fa443354124b6e9febead5f457f3539ded96
Marshallramdh <marshall.120140017@student.itera.ac.id>
2023-10-28 16:17:22 +0700

    update popup

--- resources/views/kelolasuratmasuk.blade.php
+++ resources/views/kelolasuratmasuk.blade.php
@@ -19,8 +19,43 @@
                         <span class="font-weight-bold" style="font-size: 30px;">Kelola Surat</span>
                     </div>
                     <div>
-                        <button class="btn btn-primary" style="font-size: 15px;border-radius:20px;">Tambah Surat Baru</button>
-                    </div>
+                      <button id="btnTambahSurat" class="btn btn-primary" style="font-size: 15px; border-radius: 20px;">Tambah Surat Baru</button>
+                  </div>
+                  
+                  <!-- The modal (popup) -->
+                  <div id="myModal" class="modal">
+                          <span class="close" id="closeModal">&times;</span><br>
+                          <button class="btn btn-primary">Upload Surat Masuk</button>
+                          <button class="btn btn-primary">Upload Surat Keluar</button>
+                  </div>
+                  
+                  <script>
+                  // Get the modal
+                  var modal = document.getElementById("myModal");
+                  
+                  // Get the button that opens the modal
+                  var btn = document.getElementById("btnTambahSurat");
+                  
+                  // Get the <span> element that closes the modal
+                  var span = document.getElementById("closeModal");
+                  
+                  // When the user clicks the button, open the modal
+                  btn.onclick = function() {
+                      modal.style.display = "block";
+                  }
+                  
+                  // When the user clicks on <span> (x), close the modal
+                  span.onclick = function() {
+                      modal.style.display = "none";
+                  }
+                  
+                  // When the user clicks anywhere outside of the modal, close it
+                  window.onclick = function(event) {
+                      if (event.target == modal) {
+                          modal.style.display = "none";
+                      }
+                  }
+                  </script>
                 </div>
                 <br>
             <div class="row py-6">
