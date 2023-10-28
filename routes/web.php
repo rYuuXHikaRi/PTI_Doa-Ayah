541727fc5eb28a6e4b4e5442bcbdffb250ac551e
Marshallramdh <marshall.120140017@student.itera.ac.id>
2023-10-28 15:04:04 +0700

    kelolasuratkeluar

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::get('/kelolasuratmasuk', function () {
     return view('kelolasuratmasuk');
 });
 
+Route::get('/kelolasuratkeluar', function () {
+    return view('kelolasuratkeluar');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
