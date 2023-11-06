26488a7f2240a3e37e4ccbc40ca8c94d94d70798
Ferli Andriansyah <103373638+Ferliand@users.noreply.github.com>
2023-11-06 19:57:29 +0700

    Update index.blade.php

--- resources/views/admin/SuratKeluar/index.blade.php
+++ resources/views/admin/SuratKeluar/index.blade.php
@@ -89,9 +89,6 @@
                                                         <td>
                                                             <a href=""><button class="btn btn-warning">
                                                                     <i class="fas fa-edit"></i></button></a>
-                                                            <a role="button"  class="delete-button" data-bs-toggle="modal" data-bs-target=".bd-example-modal-sm{{$suratkeluarr->id}}"><button
-                                                                class="btn btn-danger" data-toggle="modal" data-target="#hapusModal">
-                                                                    <i class="fas fa-trash"></i></button></a>
                                                             {{-- <a href="{{ route('arsipdownload', ['file' => $arsip->file , 'id' => $suratkeluarr->id]) }}"><button class="btn btn-success"><i
                                                                         class="fas fa-download"></i></button></a> --}}
 
@@ -101,28 +98,47 @@
                                                                     class="fas fa-download"></i></a>
                                                         @endif --}}
 
-                                                        <div class="modal fade bd-example-modal-sm{{$suratkeluarr->id}}" tabindex="-1" role="dialog" aria-hidden="true">
-                                                            <div class="modal-dialog">
-                                                                <div class="modal-content">
-                                                                    <div class="modal-header">
-                                                                        <h5 class="modal-title"><strong>Hapus Data</strong></h5>
-                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal">
-                                                                        </button>
-                                                                    </div>
-                                                                    <div class="modal-body">Apakah anda yakin ingin menghapus data?</div>
-                                                                    <div class="modal-footer" style="left:0px; height: 80px;">
-                                                                        <form action="{{route('suratkeluar.destroy', $suratkeluarr->id)}}" method="POST">
-                                                                        @method('DELETE')
-                                                                        @csrf
-                                                                        <div style="display: flex; justify-content: space-between;">
-                                                                            <button type="button" class="btn submit-btn submit-btn-yes" data-bs-dismiss="modal" style="width: 49%;">Tidak</button>
-                                                                            <input type="submit" class="btn submit-btn submit-btn-no" name="" id="" value="Hapus" style="width: 49%;">
-                                                                        </div>
+                                                            <a role="button" class="delete-button" data-bs-toggle="modal"
+                                                                data-bs-target=".bd-example-modal-sm{{ $suratkeluarr->id }}">
+                                                                <button class="btn btn-danger">
+                                                                    <i class="fas fa-trash"></i>
+                                                                </button>
+                                                            </a>
 
-                                                                        </form>
+                                                            <div class="modal fade bd-example-modal-sm{{ $suratkeluarr->id }}"
+                                                                tabindex="-1" role="dialog" aria-hidden="true">
+                                                                <div class="modal-dialog">
+                                                                    <div class="modal-content">
+                                                                        <div class="modal-header">
+                                                                            <h5 class="modal-title"><strong>Hapus
+                                                                                    Data</strong></h5>
+                                                                            <button type="button" class="btn-close"
+                                                                                data-bs-dismiss="modal"></button>
+                                                                        </div>
+                                                                        <div class="modal-body">Apakah anda yakin ingin
+                                                                            menghapus data?
+                                                                        <span class="badge bg-secondary" >{{$suratkeluarr->nama_surat}}</span></div>
+                                                                        <div class="modal-footer"
+                                                                            style="left:0px; height: 80px;">
+                                                                            <form
+                                                                                action="{{ route('suratkeluar.destroy', $suratkeluarr->id) }}"
+                                                                                method="POST">
+                                                                                @method('DELETE')
+                                                                                @csrf
+                                                                                <div
+                                                                                    style="display: flex; justify-content: space-between;">
+                                                                                    <button type="button"
+                                                                                        class="btn submit-btn submit-btn-yes"
+                                                                                        data-bs-dismiss="modal"
+                                                                                        style="width: 49%;">Tidak</button>
+                                                                                    <input type="submit"
+                                                                                        class="btn submit-btn submit-btn-no"
+                                                                                        value="Hapus" style="width: 49%;">
+                                                                                </div>
+                                                                            </form>
+                                                                        </div>
                                                                     </div>
                                                                 </div>
-                                                            </div>
                                                             </div>
                                                         </td>
                                                     </tr>
@@ -138,12 +154,16 @@
             </div>
 
 
-            <!-- Sisipkan script untuk jQuery -->
-            <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
             <!-- Sisipkan script untuk DataTables -->
             <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
             <!-- Sisipkan script untuk file JavaScript Anda -->
             <script src="js/kelolasuratkeluar.js"></script>
+            <!-- Example: Loading Bootstrap and Popper.js from CDN -->
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+
     </body>
 
     </html>
