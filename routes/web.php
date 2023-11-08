cf83d95b666a2a34b85fd246b4e34dca348ca649
Muhammad Ibnu Prayogi <89898918+IbnuPrayogi@users.noreply.github.com>
2023-11-08 15:22:27 +0700

    clear route

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,8 @@ Route::resource('arsip', ArsipController::class);
 Route::resource('user', UserController::class);
 Route::get('/arsip/{id}/{file}', [ArsipController::class, 'downloadarsip'])->name('arsipdownload');
 
-
-Route::get('/templatesurat', function () {
-    return view('templatesurat');
-});
-
-Route::get('/kelolasuratmasuk', function () {
-    return view('kelolasuratmasuk');
-});
-
-Route::get('/buatsuratmasuk', function () {
-    return view('buatsuratmasuk');
-});
-
-Route::get('/perubahansuratmasuk', function () {
-    return view('perubahansuratmasuk');
-});
-
 Route::resource('suratmasuk',SuratMasukController::class);
 Route::get('/suratmasuk/{id}/{file}', [SuratMasukController::class, 'downloadsuratmasuk'])->name('suratmasukdownload');
-
 
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
