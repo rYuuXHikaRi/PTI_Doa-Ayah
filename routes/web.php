e0de43de70484284c09a79472cb1d4e0445cfba2
Muhammad Ibnu Prayogi <89898918+IbnuPrayogi@users.noreply.github.com>
2023-11-09 15:12:11 +0700

    disposisi, CRUD surat keluar

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,30 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\SuratKeluarController;
-use App\Http\Controllers\ArsipController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArsipController;
+use App\Http\Controllers\DisposisiController;
 use App\Http\Controllers\SuratMasukController;
+use App\Http\Controllers\SuratKeluarController;
 
 Route::resource('arsip', ArsipController::class);
 Route::resource('user', UserController::class);
+Route::resource('dispost', DisposisiController::class);
 Route::get('/arsip/{id}/{file}', [ArsipController::class, 'downloadarsip'])->name('arsipdownload');
 
 Route::resource('suratmasuk',SuratMasukController::class);
-Route::get('/suratmasuk/{id}/{file}', [SuratMasukController::class, 'downloadsuratmasuk'])->name('suratmasukdownload');
+Route::get('/suratmasuk/download/{id}/{file}', [SuratMasukController::class, 'downloadsuratmasuk'])->name('suratmasukdownload');
 
 route::resource('suratkeluar', SuratKeluarController::class);
-Route::get('/suratkeluar/{id}/{file}', [SuratKeluarController::class, 'downloadSurat'])->name('Suratkeluar.download');
+Route::get('/suratkeluar/download/{id}/{file}', [SuratKeluarController::class, 'downloadSurat'])->name('suratkeluar.download');
 Route::get('/formtemplate', [SuratKeluarController::class, 'template'])->name('template');
+
+Route::get('/suratkeluar/approved/{id}', [SuratKeluarController::class,'approve'])->name('suratkeluar.approved');
+Route::get('/suratkeluar/rejected/{id}', [SuratKeluarController::class,'reject'])->name('suratkeluar.rejected');
+Route::get('/suratkeluar/restored/{id}', [SuratKeluarController::class,'restore'])->name('suratkeluar.restored');
+
+Route::post('/disposisi/add/{id}/{status}', [DisposisiController::class,'store'])->name('disposisi.tambahdisposisi');
+// Route::put('/suratkeluar/rejected/{id}', [SuratKeluarController::class,'tolaksurat'])->name('suratkeluar.tolak');
 
 
 // Route::get('/template', function () {
@@ -27,6 +35,6 @@ Route::get('/formtemplate', [SuratKeluarController::class, 'template'])->name('t
 
 
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
