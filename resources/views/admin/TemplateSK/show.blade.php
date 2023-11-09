97600e77b41bd0ca79aea0c4c32fedb484c9bb54
Ferli Andriansyah <103373638+Ferliand@users.noreply.github.com>
2023-11-09 13:12:29 +0700

    Update show.blade.php

--- resources/views/admin/TemplateSK/show.blade.php
+++ resources/views/admin/TemplateSK/show.blade.php
@@ -12,44 +12,45 @@
 </head>
 <body>
 
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-8 mx-auto">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
 
-        <div class="text-center mb-4">
-          <h2>Undangan</h2>
+                <div class="text-center mb-4">
+                    <h2>Undangan</h2>
+                </div>
+
+                <p class="font-weight-bold">Perihal: {{ $request->perihal }}</p>
+
+                <p>Kepada Yth,<br>
+                    Daftar Peserta Terlampir<br>
+                    di<br>
+                    Tempat
+                </p>
+
+                <p>Assalamu’alaikum Wr. Wb.</p>
+
+                <p>Melalui Surat ini Kami bermaksud mengundang Bapak/Ibu/Sdr untuk mengikuti Simulasi Pemadam Kebakaran pada :</p>
+
+                <ul>
+                    <li>Hari/Tanggal : {{ $request->hari_tanggal }}</li>
+                    <li>Waktu : {{ $request->waktu }}</li>
+                    <li>Tempat : {{ $request->tempat }}</li>
+                </ul>
+
+                <p>Mengingat pentingnya acara tersebut, maka Kami mengharapkan Bapak/Ibu/Sdr. dapat meluangkan waktu untuk datang tepat pada waktunya.</p>
+
+                <p>Demikian Memo ini Kami sampaikan, atas kehadirannya Kami ucapkan terima kasih.</p>
+
+                <p>Wassalamu’alaikum Wr. Wb.</p>
+
+                <p class="text-right">Bandar Jaya, {{ $request->tanggal_surat }}<br>
+                    Direktur RS. Islam Asy-Syifaa</p>
+
+            </div>
         </div>
-
-        <p class="font-weight-bold">Perihal: Undangan</p>
-
-        <p>Kepada Yth,<br>
-          Daftar Peserta Terlampir<br>
-          di<br>
-          Tempat
-        </p>
-
-        <p>Assalamu’alaikum Wr. Wb.</p>
-
-        <p>Melalui Surat ini Kami bermaksud mengundang Bapak/Ibu/Sdr untuk mengikuti Simulasi Pemadam Kebakaran pada :</p>
-
-        <ul>
-          <li>Hari/Tanggal : Rabu, 11 Oktober 2023</li>
-          <li>Waktu : Pukul 09.00 WIB (Ontime) s/d selesai</li>
-          <li>Tempat : Lahan Belakang RSI</li>
-        </ul>
-
-        <p>Mengingat pentingnya acara tersebut, maka Kami mengharapkan Bapak/Ibu/Sdr. dapat meluangkan waktu untuk datang tepat pada waktunya.</p>
-
-        <p>Demikian Memo ini Kami sampaikan, atas kehadirannya Kami ucapkan terima kasih.</p>
-
-        <p>Wassalamu’alaikum Wr. Wb.</p>
-
-        <p class="text-right">Bandar Jaya, 10 Oktober 2023<br>
-        Direktur RS. Islam Asy-Syifaa</p>
-
-      </div>
     </div>
-  </div>
+
 
   <!-- Bootstrap JS and dependencies -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
