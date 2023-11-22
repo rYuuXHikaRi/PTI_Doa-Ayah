24101ba17b0d5df3aac65f6cfa1d1e91b9cefb0e
Muhammad Ibnu Prayogi <89898918+IbnuPrayogi@users.noreply.github.com>
2023-11-22 20:18:13 +0700

    route clear

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ use App\Http\Controllers\SuratMasukController;
 use App\Http\Controllers\SuratKeluarController;
 use App\Http\Controllers\KepalaBagian\KBSuratKeluarController;
 
+
+Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
 Route::middleware(['auth','role:1'])->group(function () {
     Route::resource('arsip', ArsipController::class);
     Route::get('/arsip/{id}/{file}', [ArsipController::class, 'downloadarsip'])->name('arsipdownload');
@@ -30,6 +34,16 @@ Route::middleware(['auth','role:1'])->group(function () {
     Route::get('/suratkeluar/approved/{id}', [SuratKeluarController::class,'approve'])->name('suratkeluar.approved');
     Route::get('/suratkeluar/rejected/{id}', [SuratKeluarController::class,'reject'])->name('suratkeluar.rejected');
     Route::get('/suratkeluar/restored/{id}', [SuratKeluarController::class,'restore'])->name('suratkeluar.restored');
+
+    route::resource('templateSK', TemplateSKController::class);
+    Route::get('/templateSk', [TemplateSKController::class, 'showDesk'])->name('templateSK.showDesk');
+    // Route::post('/templateSk/{id}', [TemplateSKController::class, 'template'])->name('templateSK.template');
+    // Route::post('/templateSk/{id}', [TemplateSKController::class, 'storeTemplate'])->name('templateSK.storeSK');
+    Route::post('/templateSk', [TemplateSKController::class, 'storeSKnew'])->name('templateSK.storeSKnew');
+    Route::post('/templateSk/{id}', [TemplateSKController::class, 'storeSKForm'])->name('templateSK.storeSKForm');
+    Route::get('/template/priview/{id}', [TemplateSKController::class, 'priview'])->name('templateSK.priview');
+    Route::put('/template/priview/{id}', [TemplateSKController::class, 'Sign'])->name('templateSK.Sign');
+    // Route::get('/suratkeluar/{id}', [TemplateSKController::class, 'signature'])->name('templateSK.signature');
 });
 
 Route::middleware(['auth','role:2'])->group(function () {
@@ -57,63 +71,5 @@ Route::middleware(['auth','role:2'])->group(function () {
 
 
 
-// Route::get('/template', function () {
-//         return view('admin.TemplateSK.template');
-//     });
-
-// Route::get('/buatsurattemplate', function () {
-//     return view('buatsurattemplate');
-// });
-// Route::get('/dashboardkabag', function () {
-//     return view('dashboardkabag');
-// });
-
-// Route::get('/daftarpermohonansuratmasuk', function () {
-//     return view('daftarpermohonansuratmasuk');
-// });
-
-// Route::get('/daftarpermohonansuratkeluar', function () {
-//     return view('daftarpermohonansuratkeluar');
-// });
-
-// Route::get('/daftarpermohonanizin', function () {
-//     return view('daftarpermohonanizin');
-// });
-
-// Route::get('/daftarpermohonancuti', function () {
-//     return view('daftarpermohonancuti');
-// });
-
-// Route::get('/daftarpermohonantukarjaga', function () {
-//     return view('daftarpermohonantukarjaga');
-// });
-// Auth::routes();
-// Route::get('/suratkeluar/download/{id}/{file}', [SuratKeluarController::class, 'downloadSurat'])->name('suratkeluar.download');
-// Route::get('/formtemplate', [SuratKeluarController::class, 'template'])->name('template');
-
-// Route::get('/suratkeluar/approved/{id}', [SuratKeluarController::class,'approve'])->name('suratkeluar.approved');
-// Route::get('/suratkeluar/rejected/{id}', [SuratKeluarController::class,'reject'])->name('suratkeluar.rejected');
-// Route::get('/suratkeluar/restored/{id}', [SuratKeluarController::class,'restore'])->name('suratkeluar.restored');
 
 
-
-
-
-
-
-
-
-
-// // Route::put('/suratkeluar/rejected/{id}', [SuratKeluarController::class,'tolaksurat'])->name('suratkeluar.tolak');
-
-
-// Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// // Route::get('/template', function () {
-// //     return view('admin.suratkeluar.template');
-// // });
-
-
-
-// Auth::routes();
-
-// // Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
