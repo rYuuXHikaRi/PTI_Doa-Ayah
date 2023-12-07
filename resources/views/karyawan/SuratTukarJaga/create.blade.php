58ca1f8b8bace8b7f0f8d8215ae83671a174e36a
Edo Sani <sani140402@gmail.com>
2023-12-07 23:44:39 +0700

    h

--- resources/views/karyawan/SuratTukarJaga/create.blade.php
+++ resources/views/karyawan/SuratTukarJaga/create.blade.php
@@ -5,36 +5,32 @@
     <link rel="stylesheet" href="../css/tukarjagamobile.css">
 
     <div class="container">
-        <div class="card-header">
-            <h1><b>Ajukan Tukar Jaga</b></h1>
-        </div>
-        <div class="card-body">
-            <form method="post" action="{{route('surattukarjaga.store')}}" enctype="multipart/form-data">
-                @csrf
+        <form method="post" action="{{route('surattukarjaga.store')}}" enctype="multipart/form-data">
+            @csrf
+            <div class="card-header">
+                <h1><b>Ajukan Tukar Jaga</b></h1>
+            </div>
+            <div class="card-body">
                 <div class="content-text">
-                    <h3>Jadwal asli</h3>
+                    <h3>Jadwal Asli</h3>
                 </div>
                 <div class="content-box">
-                    <input class="input_waktu" type="date" id="Tanggal_Mulai" name="tanggal_mulai">
-                <div class="box">
-                    <input class="input_waktu" type="date" id="Tanggal_asli" placeholder="Tanggal Pengajuan..."
-                        name="jadwal_asli"  onchange="checkDate('Tanggal_asli')">
+                    <input class="input_waktu" type="date" id="Tanggal_Pengajuan" placeholder="Tanggal Pengajuan..."
+                        name="jadwal_asli">
                 </div>
-    
+
                 <div class="content-text">
-                    <h3>Jadwal yang ingin diubah</h3>
+                    <h3>Jadwal Yang Ingin Diubah</h3>
                 </div>
                 <div class="content-box">
-                    <input class="input_waktu" type="date" id="Tanggal_izin" name="tanggal_selesai">
-                <div class="box">
-                    <input class="input_waktu" type="date" id="Tanggal_diubah" placeholder="Tanggal Target..." name="jadwal_dirubah" onchange="checkDate('Tanggal_diubah')">
+                    <input class="input_waktu" type="date" id="Tanggal_Target" placeholder="Tanggal Target..." name="jadwal_dirubah">
                 </div>
-    
+
                 <div class="content-text">
-                    <h3>Nama target</h3>
+                    <h3>Nama Target</h3>
                 </div>
                 <div class="dropdown-menu">
-                    <select class="select">
+                    <select class="select-menu">
                         <option value="">Pilih Nama Target</option>
                         <option value="first">Astra</option>
                         <option value="second">Brimstone</option>
@@ -44,42 +40,40 @@
                         <i class='bx bx-chevron-down'></i>
                     </div>
                 </div>
-    
+
                 <div class="content-text">
                     <h3>Keterangan</h3>
                 </div>
                 <div class="content-ket">
-                    <textarea class="input_ket" type="text" id="keterangan" placeholder="Alasan..." name="keterangan"></textarea>
+                    <textarea class="input_ket" type="text" id="keterangan" placeholder="Keterangan..." name="keterangan"></textarea>
                 </div>
-    
+
                 <!-- Button trigger modal -->
-                <button type="submit" onclick="notifSukses()" class="btn btn-primary">
+                <button onclick="togglePopup()" class="btn btn-primary">
                     Buat Pengajuan
                 </button>
-            </form>
+        </form>
 
-            <div id="overlay_berhasil" class="overlay_berhasil" style="display: none;"></div>
-            <div class="notif_berhasil" style="display: none;">
-                <div class="info_pengajuan">
-                    <h1><b>Pengajuan Tukar Jaga Berhasil Dibuat</b></h1>
-                </div>
-                <div class="icon_sukses">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 120 120" fill="none">
-                        <circle cx="60" cy="60" r="60" fill="#0253BA" />
+        <div id="overlay_berhasil" class="overlay_berhasil" style="display: none;"></div>
+        <div class="notif_berhasil" style="display: none;">
+            <div class="info_pengajuan">
+                <h1><b>Pengajuan Tukar Jaga Berhasil Dibuat</b></h1>
+            </div>
+            <div class="icon_sukses">
+                <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 120 120" fill="none">
+                    <circle cx="60" cy="60" r="60" fill="#0253BA" />
+                </svg>
+                <div class="ceklis">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="67" viewBox="0 0 80 67"
+                        fill="none">
+                        <path
+                            d="M28.4408 50.124L11.3807 33.0638C8.7772 30.4603 4.55611 30.4603 1.95262 33.0638C-0.650872 35.6673 -0.650872 39.8884 1.95262 42.4919L24.1748 64.7141C26.9468 67.4861 31.5006 67.2795 34.0103 64.2679L78.4546 10.9347C80.8117 8.1062 80.4296 3.90244 77.6011 1.54536C74.7726 -0.811733 70.5688 -0.429574 68.2117 2.39893L28.4408 50.124Z"
+                            fill="#E6EFFA" />
                     </svg>
-                    <div class="ceklis">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="67" viewBox="0 0 80 67"
-                            fill="none">
-                            <path
-                                d="M28.4408 50.124L11.3807 33.0638C8.7772 30.4603 4.55611 30.4603 1.95262 33.0638C-0.650872 35.6673 -0.650872 39.8884 1.95262 42.4919L24.1748 64.7141C26.9468 67.4861 31.5006 67.2795 34.0103 64.2679L78.4546 10.9347C80.8117 8.1062 80.4296 3.90244 77.6011 1.54536C74.7726 -0.811733 70.5688 -0.429574 68.2117 2.39893L28.4408 50.124Z"
-                                fill="#E6EFFA" />
-                        </svg>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-@endsection
     </div>
 @endsection
 
